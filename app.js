91e468efbaa8d4aaab41a664148990726b8f19e7
Bruno HUDBERT <dev@bruno.hudbert.fr>
2020-12-08 12:16:05 +0100

    starting training's project

--- app.js
+++ app.js
@@ -1,28 +1,35 @@
-var createError = require('http-errors');
+// Require des modules natifs
 var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var path = require('path');
 
+// Require spécifique
+var cookieParser = require('cookie-parser');
+var createError = require('http-errors');
+
+// Require des Routers
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+
 
 var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
+app.set('view engine', 'pug');
 
-app.engine
+//middleware pour tous les path
 
-app.use(logger('dev'));
+app.use(logger('short'));
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+//middleware pour  les path specifiques
+
+
+app.use(indexRouter);
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
